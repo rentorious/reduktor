b308c931931b7b7042fa146a9a74a3fe201558db
Ognjen Popovic <rentorious@gmail.com>
2020-08-02 18:56:25 +0200

    transformation pipeline

--- reduktor/astro_scripts/helpers.py
+++ reduktor/astro_scripts/helpers.py
@@ -3,6 +3,7 @@ import re
 import reduktor.astro_scripts.transformations as trans
 import json
 
+# Coordinate system names
 NEBESKO_EKVATORSKI = "Nebesko ekvatorski"
 MESNO_EKVATORSKI = "Mesno Ekvatorski"
 EKLIPTICKI = "Ekliptički"
@@ -17,6 +18,8 @@ E_LAT = "latituda"
 E_LONG = "longituda"
 AZIM = "azimut"
 ZEN_DIST = "zenitksa daljina"
+
+GEO_TOPO = "GEO_TOPO"
 
 system_informations = {
     NEBESKO_EKVATORSKI: {
@@ -126,36 +129,86 @@ def get_all_systems_info():
     return system_informations
 
 
-# TODO: IMPLEMENT EQUATORIAL
 # Ovo ce biti pakleno
-def transform_system(data):
+def transform_system(data, options):
     start_name = data["startName"]
     start_data = data["startData"]
-    endName = data["endName"]
+    end_name = data["endName"]
 
     # Check if the names are valid
-    if start_name not in system_informations or endName not in system_informations:
+    if start_name not in system_informations or end_name not in system_informations:
         raise ValueError("Unknown coordinate system")
 
     # Transform any coordinate system into Celestial equatorial
     ra, dec = any_to_cel(start_name, start_data)
-
-
-    result = {}
+    # Adjust coordinates for nutation, precession...
+    ra, dec = adjust_to_now(ra, dec, options)
+    # Transform those coordinates to the output system
+    result = cel_to_any(end_name, ra, dec, start_data)
 
 
     # Serialize the results
-    for key in result:
+    result = serialize_trans_result(result)
+
+    return result
+
+
+
+def any_to_cel(sys_name, sys_data):
+    star_time = sys_data[STAR_TIME]
+    lat = sys_data[LAT]
+
+    result = {}
+
+    if sys_name == NEBESKO_EKVATORSKI:
+        result[RA] = sys_data[RA]
+        result[DEC] = sys_data[DEC]
+    elif sys_name == MESNO_EKVATORSKI:
+        result[HOUR], result[DEC] = trans.eql_loc_to_cel(sys_data[HOUR], sys_data[DEC], star_time)
+    elif sys_name == HORIZONTSKI:
+        result[AZIM], result[ZEN_DIST] = trans.hor_to_cel_eql(sys_data[AZIM], sys_data[ZEN_DIST], star_time, lat)
+    elif sys_name == EKLIPTICKI:
+        result[E_LONG], result[E_LAT] = trans.ecl_to_cel_eql()
+    else:
+        raise ValueError("Unknown coordinate system (any_to_cel)")
+
+    return result
+
+
+def cel_to_any(sys_name, ra, dec, start_data):
+    if sys_name == NEBESKO_EKVATORSKI:
+        return  ra, dec
+    if sys_name == MESNO_EKVATORSKI:
+        return trans.eql_cel_to_loc(ra, dec, start_data[STAR_TIME])
+    if sys_name == HORIZONTSKI:
+        return trans.cel_eql_to_hor(ra, dec, start_data[STAR_TIME], start_data[LAT])
+    if sys_name == EKLIPTICKI:
+        return trans.cel_eql_to_ecl()
+
+    raise ValueError("Unknown system (cel_to_any)")
+
+
+def adjust_to_now(ra, dec, options):
+    if GEO_TOPO in options:
+        r = options[GEO_TOPO]["r"]
+        h = options[GEO_TOPO]["h"]
+        lat = options[GEO_TOPO]["lat"]
+        lon = options[GEO_TOPO]["lon"]
+
+        ra, dec = trans.geo_topo(ra, dec, r, lat, lon, h)
+
+    # TODO: Implement nutation, precession and abberation
+
+    return ra, dec
+
+
+def serialize_trans_result(result_dict):
+    for key in result_dict:
         hour = False
         # check if angle should be serialized to hour values
         if key == HOUR or key == RA or key == STAR_TIME:
             hour = True
 
-        result[key] = serialize(result[key], hour=hour)
+        result_dict[key] = serialize(result_dict[key], hour=hour)
 
-    print(json.dumps(result))
-    return json.dumps(result)
-
-def any_to_cel(sys_name, sys_data):
-    star_time = sys_data[STAR_TIME]
-    lat = sys_data[LAT]
+    return json.dumps(result_dict)
